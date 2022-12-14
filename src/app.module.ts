54e786e1756c973de71946cf020cbcc8237befdd
amaralc <amaral.calil@gmail.com>
2022-12-14 11:06:32 -0300

    feat: add http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { HttpModule } from './http.module';
 import { MailService } from './mail/mail.service';
 import { SMTPMailService } from './mail/smtp-mail.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [
     // Declaration of providers with abstract classes
